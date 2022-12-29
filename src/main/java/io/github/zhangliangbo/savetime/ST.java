2d7dc0b3393cff0d445b4b1fe9c62954ecbdd4ad
zhangliangbo <2472110501@qq.com>
2022-12-29 18:23:19 +0800

    auto push 20221229

--- src/main/java/io/github/zhangliangbo/savetime/ST.java
+++ src/main/java/io/github/zhangliangbo/savetime/ST.java
@@ -25,6 +25,7 @@ public final class ST {
     public static final Rabbit rabbit = new Rabbit();
     public static final Rand rand = new Rand();
     public static final MybatisPlusGenerator mybatisPlusGenerator = new MybatisPlusGenerator();
+    public static final Vonage vonage = new Vonage();
 
     public static <T> List<T> listOf(Iterable<T> iterable) {
         return List.ofAll(iterable);
---
+++ src/main/java/io/github/zhangliangbo/savetime/inner/Vonage.java
@@ -0,0 +1,11 @@
+package io.github.zhangliangbo.savetime.inner;
+
+/**
+ * https://developer.vonage.com/
+ *
+ * @author zhangliangbo
+ * @since 2022/12/29
+ */
+public class Vonage extends Http {
+    
+}
