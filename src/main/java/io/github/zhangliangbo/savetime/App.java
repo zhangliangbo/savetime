1658a366e11e3a901fca8f2cb02ea8315acb3e8a
zhangliangbo <2472110501@qq.com>
2023-02-05 22:22:58 +0800

    auto push 20230205

--- src/main/java/io/github/zhangliangbo/savetime/App.java
+++ src/main/java/io/github/zhangliangbo/savetime/App.java
@@ -8,7 +8,7 @@ import java.util.concurrent.CompletableFuture;
  */
 public class App {
     public static void main(String[] args) throws Exception {
-        CompletableFuture<Void> completableFuture = ST.netty.snoopServer(9999, false);
+        CompletableFuture<Void> completableFuture = ST.netty.httpStaticFileServer(9999, false);
         completableFuture.join();
     }
 }
