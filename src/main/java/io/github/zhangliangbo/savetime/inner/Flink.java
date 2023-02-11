762a2adff1c2cc4466b362561b602dbca23d989f
zhangliangbo <2472110501@qq.com>
2023-02-11 22:31:23 +0800

    auto push 20230211

---
+++ src/main/java/io/github/zhangliangbo/savetime/inner/Flink.java
@@ -0,0 +1,11 @@
+package io.github.zhangliangbo.savetime.inner;
+
+/**
+ * /jobs/overview
+ * /jobs/e62cefa45693b8337b3b8504dde8a002
+ * @author zhangliangbo
+ * @since 2023/2/11
+ */
+public class Flink extends Http {
+
+}
