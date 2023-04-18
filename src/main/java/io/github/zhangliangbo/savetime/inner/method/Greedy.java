d670a879c9f04de914b724db3acd678e40af3c3f
zhangliangbo <2472110501@qq.com>
2023-04-18 18:25:40 +0800

    auto push 20230418

--- src/main/java/io/github/zhangliangbo/savetime/inner/method/Greedy.java
+++ src/main/java/io/github/zhangliangbo/savetime/inner/method/Greedy.java
@@ -2,13 +2,47 @@ package io.github.zhangliangbo.savetime.inner.method;
 
 import io.github.zhangliangbo.savetime.inner.problem.MaxSubArray;
 
+import java.util.Arrays;
+
 /**
  * @author zhangliangbo
  * @since 2023/4/17
  */
 public class Greedy implements MaxSubArray {
+    public static void main(String[] args) {
+        Greedy greedy = new Greedy();
+        int[] ints = greedy.maxSubArray(greedy.maxSubArraySample());
+        System.out.println(Arrays.toString(ints));
+    }
+
     @Override
     public int[] maxSubArray(int[] nums) {
-        return new int[0];
+        int dp = nums[0];
+        int low = 0;
+        int high = 0;
+
+        int pre = dp;
+
+        int max = dp;
+        int maxLeft = 0;
+        int maxRight = 0;
+
+
+        for (int i = 1; i < nums.length; i++) {
+            int plus = pre + nums[i];
+            if (plus > nums[i]) {
+                dp = plus;
+                high = i;
+            } else {
+                dp = nums[i];
+                low = i;
+            }
+            if (dp > max) {
+                max = dp;
+                maxLeft = low;
+                maxRight = high;
+            }
+        }
+        return new int[]{maxLeft, maxRight, max};
     }
 }
