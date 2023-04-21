f4ab09c05c1e0d6682410260f805fdd792ca7920
zhangliangbo <2472110501@qq.com>
2023-04-21 14:47:48 +0800

    auto push 20230421

--- src/main/java/io/github/zhangliangbo/savetime/inner/Lettuce.java
+++ src/main/java/io/github/zhangliangbo/savetime/inner/Lettuce.java
@@ -3,11 +3,18 @@ package io.github.zhangliangbo.savetime.inner;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.github.zhangliangbo.savetime.ST;
+import io.lettuce.core.KeyScanCursor;
 import io.lettuce.core.RedisClient;
 import io.lettuce.core.RedisURI;
+import io.lettuce.core.ScanArgs;
 import io.lettuce.core.api.StatefulRedisConnection;
+import io.lettuce.core.api.sync.RedisCommands;
+import org.apache.commons.collections.CollectionUtils;
 
 import java.net.URI;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Consumer;
 
 /**
  * @author zhangliangbo
@@ -36,6 +43,29 @@ public class Lettuce extends AbstractConfigurable<StatefulRedisConnection<String
         redisUri.setPassword(password.toCharArray());
         RedisClient redisClient = RedisClient.create(redisUri);
         return redisClient.connect();
+    }
+
+    /**
+     * 扫描键
+     *
+     * @param key      环境
+     * @param pattern  正则表达式
+     * @param limit    每批次数量
+     * @param consumer 消费者
+     * @throws Exception 异常
+     */
+    public void scan(String key, String pattern, int limit, Consumer<List<String>> consumer) throws Exception {
+        ScanArgs scanArgs = ScanArgs.Builder.matches(pattern).limit(limit);
+        RedisCommands<String, String> sync = getOrCreate(key).sync();
+
+        KeyScanCursor<String> keyScanCursor = sync.scan(scanArgs);
+        while (!keyScanCursor.isFinished()) {
+            List<String> keys = keyScanCursor.getKeys();
+            if (CollectionUtils.isNotEmpty(keys)) {
+                consumer.accept(keys);
+            }
+            keyScanCursor = sync.scan(keyScanCursor);
+        }
     }
 
 }
