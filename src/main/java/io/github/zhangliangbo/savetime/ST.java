53aea6ada7fce5a3555fc4451f8832a915566e59
zhangliangbo <2472110501@qq.com>
2023-05-07 21:50:55 +0800

    auto push 20230507

--- src/main/java/io/github/zhangliangbo/savetime/ST.java
+++ src/main/java/io/github/zhangliangbo/savetime/ST.java
@@ -45,6 +45,7 @@ public final class ST {
     public static final DivideConquer divideConquer = new DivideConquer();
     public static final Greedy greedy = new Greedy();
     public static final Recursion recursion = new Recursion();
+    public static final Iteration iteration = new Iteration();
 
     public static <T> List<T> listOf(Iterable<T> iterable) {
         return List.ofAll(iterable);
